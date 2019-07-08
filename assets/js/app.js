bc011d1a1ab32861a7dca63f01a6ef7c377ff296
Ridick Luiz Sossela <ridick_sosssela@hotmail.com>
2019-07-08 18:52:54 -0300

    Layout completo

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,8 @@
 window.onhashchange = (e) =>{
     const oldURL = e.oldURL.split('#')[1];
     const newURL = e.newURL.split('#')[1];
-    const oldLINK = document.querySelector(`a[href='#${oldURL}']`);
-    const newLINK = document.querySelector(`a[href='#${newURL}']`);
+    const oldLINK = document.querySelector(`.navegacao a[href='#${oldURL}']`);
+    const newLINK = document.querySelector(`.navegacao a[href='#${newURL}']`);
     oldLINK && oldLINK.classList.remove('selected');
     newLINK && newLINK.classList.add('selected');
 }
